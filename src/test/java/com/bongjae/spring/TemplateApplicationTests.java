01aee95b3b42f99d0542995f390338fa00ca4eb4
BongJaeJeong <jeongbj@everyplay.co.kr>
2024-11-13 09:16:47 +0900

    스프링 부트 템플릿 초기 커밋

---
+++ src/test/java/com/bongjae/spring/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongjae.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
